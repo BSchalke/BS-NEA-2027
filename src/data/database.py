a1ec42ac955885ff73c2636b329cb6bf9f424584
BenSchalke <benjaminschalke@yahoo.co.uk>
2026-08-25 21:05:12 +0100

    Added rest of the tables

--- src/data/database.py
+++ src/data/database.py
@@ -28,15 +28,55 @@ CREATE TABLE IF NOT EXISTS venues (
     capacity INT NOT NULL
 );
 '''
-
+CREATE_ARTISTUSERS = '''
+CREATE TABLE IF NOT EXISTS artistUsers (
+    artist_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+'''
+CREATE_VENUEUSERS = '''
+CREATE TABLE IF NOT EXISTS venueUsers (
+    venue_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (venue_id) REFERENCES venues (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+'''
+CREATE_EVENTS = '''
+CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY,
+    venue_id INT NOT NULL,
+    date text NOT NULL,
+    door_time text NOT NULL,
+    FOREIGN KEY (venue_id) REFERENCES venues (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+'''
+CREATE_EVENTARTISTS = '''
+CREATE TABLE IF NOT EXISTS eventArtists (
+    artist_id INT NOT NULL,
+    event_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artists (id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE ON UPDATE CASCADE
+)
+'''
 
 def load_database(): #Creates the database and tables if it does not already exist
     with sqlite3.connect(DB_PATH) as conn:
         print("Opened database succesfully")
         cursor = conn.cursor()
+
+        #Create all tables
         cursor.execute(CREATE_ARTISTS)
         cursor.execute(CREATE_USERS)
         cursor.execute(CREATE_VENUES)
+        cursor.execute(CREATE_ARTISTUSERS)
+        cursor.execute(CREATE_VENUEUSERS)
+        cursor.execute(CREATE_EVENTS)
+        cursor.execute(CREATE_EVENTARTISTS)
+
         conn.commit()
 
     print("Database successfully loaded")
+
